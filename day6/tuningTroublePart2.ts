88bf279770d544ed95a4f2e7625b4c87a389ecfd
Ashley Hutson <ashley.hutson@mheducation.com>
2022-12-06 08:58:55 -0800

    day 6

---
+++ day6/tuningTroublePart2.ts
@@ -0,0 +1,50 @@
+// read the input from puzzleInput.txt
+// advent of code 2022 day 6 part 2
+
+import { readFileSync, writeFileSync } from "fs";
+import * as path from 'path';
+
+const readInput = async () => {
+  return await readFileSync(path.join(__dirname, "puzzleInput.txt"), "utf8");
+};
+
+const writeOutput = async (output: string) => {
+  await writeFileSync(path.join(__dirname, "TuningTroublePart2.txt"), output);
+};
+
+
+const main = async () => {
+  const input = await readInput();
+  const line = input.split("\n")[0];
+  let packetStart = 0;
+  let output: string = "";
+
+
+  for (let i = 13; i < line.length; i++) {
+    // keep a buffer of 4 characters
+    const buffer = line.substring(i - 13, i + 1);
+    //console.log(buffer);
+    if (!containsDuplicateCharacter(buffer)) {
+      packetStart = i + 1;
+      break;
+    }  
+  }
+  
+  
+
+  output = `Start of Packet: ${packetStart}`;
+  console.log(output);
+  await writeOutput(output);
+};
+
+function containsDuplicateCharacter(str1: string): boolean {
+  for (let i = 0; i < str1.length; i++) {
+    let char = str1.charAt(i);
+    if (str1.indexOf(char) != i) {
+      return true;
+    }
+  }
+  return false;
+};
+
+main();
