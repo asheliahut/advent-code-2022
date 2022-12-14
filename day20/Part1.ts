e5df9ebbf63ddddc27a01e78cbe6d9924c7ac816
Ashley Hutson <ashley.hutson@mheducation.com>
2022-12-13 23:19:22 -0800

    up to day 25 prep

---
+++ day20/Part1.ts
@@ -0,0 +1,26 @@
+// read the input from puzzleInput.txt
+// advent of code 2022 day 15 part 1
+
+import { readFileSync, writeFileSync } from "fs";
+import * as path from "path";
+
+const readInput = async () => {
+  // return await readFileSync(path.join(__dirname, "example.txt"), "utf8");
+  return await readFileSync(path.join(__dirname, "puzzleInput.txt"), "utf8");
+};
+
+const writeOutput = async (output: string) => {
+  await writeFileSync(path.join(__dirname, "Part1.txt"), output);
+};
+
+const main = async () => {
+  const input = await readInput();
+  const lines = input.split("\n");
+  let output: string = "";
+
+  output = `Output Message: ${output}`;
+  console.log(output);
+  await writeOutput(output);
+};
+
+main();
